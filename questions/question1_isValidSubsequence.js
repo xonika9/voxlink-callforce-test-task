d3dcc23879afb84bb7eb823c69b032d160dfeb1e
xonika9 <xonika9@gmail.com>
2023-05-04 09:55:51 +0300

    question 1

--- questions/question1_isValidSubsequence.js
+++ questions/question1_isValidSubsequence.js
@@ -1,8 +1,16 @@
-
 function isValidSubsequence(array, sequence) {
-    // code here
+  if (sequence.length === 0) {
+    return false;
+  }
+  let seqIndex = 0;
+  for (let i = 0; i < array.length && seqIndex < sequence.length; i++) {
+    if (array[i] === sequence[seqIndex]) {
+      seqIndex++;
+    }
+  }
+  return seqIndex === sequence.length;
 }
 
 module.exports = {
-    isValidSubsequence,
+  isValidSubsequence,
 };
--- questions/question1_isValidSubsequence_tests.js
+++ questions/question1_isValidSubsequence_tests.js
@@ -1,134 +1,132 @@
-
-const {
-    isValidSubsequence,
-} = require('./question1_isValidSubsequence');
+const { isValidSubsequence } = require('./question1_isValidSubsequence');
 
 [
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [1, 6, -1, 10]
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [5, 1, 22, 25, 6, -1, 8, 10]
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [5, 1, 22, 6, -1, 8, 10]
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [22, 25, 6]
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [1, 6, 10]
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [5, 1, 22, 10]
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [5, -1, 8, 10]
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [25]
-    },
-    {
-        "array": [1, 1, 1, 1, 1],
-        "sequence": [1, 1, 1]
-    },
-    {
-        "array": [],
-        "sequence": [1, 1, 1],
-        "result": false
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [],
-        "result": false
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [5, 1, 22, 25, 6, -1, 8, 10, 12],
-        "result": false
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [4, 5, 1, 22, 25, 6, -1, 8, 10],
-        "result": false
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [5, 1, 22, 23, 6, -1, 8, 10],
-        "result": false
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [5, 1, 22, 22, 25, 6, -1, 8, 10],
-        "result": false
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [5, 1, 22, 22, 6, -1, 8, 10],
-        "result": false
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [1, 6, -1, -1],
-        "result": false
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [1, 6, -1, -1, 10],
-        "result": false
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [1, 6, -1, -2],
-        "result": false
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [26],
-        "result": false
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [5, 1, 25, 22, 6, -1, 8, 10],
-        "result": false
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [5, 26, 22, 8],
-        "result": false
-    },
-    {
-        "array": [1, 1, 6, 1],
-        "sequence": [1, 1, 1, 6],
-        "result": false
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [1, 6, -1, 10, 11, 11, 11, 11],
-        "result": false
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [5, 1, 22, 25, 6, -1, 8, 10, 10],
-        "result": false
-    },
-    {
-        "array": [5, 1, 22, 25, 6, -1, 8, 10],
-        "sequence": [1, 6, -1, 5],
-        "result": false
-    }
-].forEach(test => {
-    const { result = true } = test;
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [1, 6, -1, 10],
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [5, 1, 22, 25, 6, -1, 8, 10],
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [5, 1, 22, 6, -1, 8, 10],
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [22, 25, 6],
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [1, 6, 10],
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [5, 1, 22, 10],
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [5, -1, 8, 10],
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [25],
+  },
+  {
+    array: [1, 1, 1, 1, 1],
+    sequence: [1, 1, 1],
+  },
+  {
+    array: [],
+    sequence: [1, 1, 1],
+    result: false,
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [],
+    result: false,
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [5, 1, 22, 25, 6, -1, 8, 10, 12],
+    result: false,
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [4, 5, 1, 22, 25, 6, -1, 8, 10],
+    result: false,
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [5, 1, 22, 23, 6, -1, 8, 10],
+    result: false,
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [5, 1, 22, 22, 25, 6, -1, 8, 10],
+    result: false,
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [5, 1, 22, 22, 6, -1, 8, 10],
+    result: false,
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [1, 6, -1, -1],
+    result: false,
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [1, 6, -1, -1, 10],
+    result: false,
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [1, 6, -1, -2],
+    result: false,
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [26],
+    result: false,
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [5, 1, 25, 22, 6, -1, 8, 10],
+    result: false,
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [5, 26, 22, 8],
+    result: false,
+  },
+  {
+    array: [1, 1, 6, 1],
+    sequence: [1, 1, 1, 6],
+    result: false,
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [1, 6, -1, 10, 11, 11, 11, 11],
+    result: false,
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [5, 1, 22, 25, 6, -1, 8, 10, 10],
+    result: false,
+  },
+  {
+    array: [5, 1, 22, 25, 6, -1, 8, 10],
+    sequence: [1, 6, -1, 5],
+    result: false,
+  },
+].forEach((test) => {
+  const { result = true } = test;
+  // console.log(result);
 
-    if (isValidSubsequence(test.array, test.sequence) !== result) {
-        throw new Error('Test failed');
-    }
+  if (isValidSubsequence(test.array, test.sequence) !== result) {
+    throw new Error('Test failed');
+  }
 });
